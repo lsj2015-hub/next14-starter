4813670f867719782ffd38c99e79c148231b585d
lsj2015-hub <lsj2015@hotmail.com>
2024-04-06 16:51:55 +0800

    Making basic structure of site

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+
+import styles from './about.module.css';
+
 const AboutPage = () => {
-  return <div>About page</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/17920141/pexels-photo-17920141.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import styles from './navLinks.module.css';
+
+const NavLink = ({ item }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathname === item.path && styles.active
+      }`}
+    >
+      {item.title}
+    </Link>
+  );
+};
+
+export default NavLink;
