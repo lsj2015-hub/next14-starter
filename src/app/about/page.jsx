18e4a1b08800494cc504c8f69b0e8e30a5151e36
lsj2015-hub <lsj2015@hotmail.com>
2024-04-06 19:46:30 +0800

    About Page Design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,13 +4,35 @@ import styles from './about.module.css';
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subTitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of exprience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of exprience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of exprience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/17920141/pexels-photo-17920141.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-          alt="about"
-          fill
-        />
+        <Image src="/about.png" alt="About Image" fill className={styles.img} />
       </div>
     </div>
   );
