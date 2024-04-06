84ad1c9d9917cc418e749ba33a46e40106800722
lsj2015-hub <lsj2015@hotmail.com>
2024-04-06 17:22:27 +0800

    Homepage Design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,35 @@
+import Image from 'next/image';
+import styles from './home.module.css';
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+          culpa ipsam provident, in laboriosam, ab dolorem ratione quisquam
+          distinctio debitis, expedita impedit a. Voluptas labore placeat
+          quaerat corporis exercitationem obcaecati.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt="brands"
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="hero" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
