c47feab92b4d322cdc370fff1b8b5a052a146597
lsj2015-hub <lsj2015@hotmail.com>
2024-04-07 21:09:45 +0800

    Suspense

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import styles from './singlePost.module.css';
+import PostUser from '@/components/postUser/postUser';
+import { Suspense } from 'react';
 
 const getData = async (slug) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
@@ -36,10 +38,9 @@ const SinglePostpage = async ({ params }) => {
             height={50}
             className={styles.avatar}
           />
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>Terry Jefferson</span>
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostUser userId={post.userId} />
+          </Suspense>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>01.01.2024</span>
