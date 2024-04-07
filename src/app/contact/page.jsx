f5176aba3440383aca01641b9424818f240d5de0
lsj2015-hub <lsj2015@hotmail.com>
2024-04-07 18:52:11 +0800

    Hydration Error and the solutions

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,27 @@
+'use client';
+
 import Image from 'next/image';
+// import dynamic from 'next/dynamic';
+
 import styles from './contact.module.css';
+// import HydrationTest from '@/components/hydrationTest';
+
+// const HydrationTestNoSSR = dynamic(() => import('@/components/hydrationTest'), {
+//   ssr: false,
+// });
 
 const ContactPage = () => {
+  const a = Math.random();
+  console.log(a);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="Contact" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+        {/* <HydrationTestNoSSR /> */}
+        <div suppressHydrationWarning>{a}</div>
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname" />
           <input type="text" placeholder="Email Address" />
