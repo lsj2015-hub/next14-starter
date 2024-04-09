b598fe5cc6e9c179862b5fd3e599f8e15ea156d8
lsj2015-hub <lsj2015@hotmail.com>
2024-04-09 21:21:48 +0800

    Next Auth Social Media Sign in How to Add User To DB

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { connectToDb } from './utils';
+import { User } from './models';
+import { revalidatePath } from 'next/cache';
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +16,29 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log(user, account, profile);
+      if (account.provider === 'github') {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              img: profile.avatar_url,
+            });
+
+            await newUser.save();
+          }
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
